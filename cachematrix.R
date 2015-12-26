6013264a2d051429b5572aeb5da39d5256d34197
antonio <antoniohps@gmail.com>
2015-12-26 16:10:06 -0200

    Verion to be peer-graded

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions in this file allows the manipulation of a special 
+## matrix object that can store a matrix elements and its inverse,
+## caching the inversion operation result so that we can use the 
+## it as many times as we want withou recomputing it. 
 
-## Write a short comment describing this function
-
+## Create the special matrix that can cache its inverse.
+## The makeCacheMatrix function actually builds a list containing
+## functions that set or retrieve the cached matrix elements and
+## its inverse. Both cached matrix and inverse is stored in the
+## environment created by the makeCacheMatrix function call.
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  ## x must be a matrix
+  stopifnot(is.matrix(x))
+  
+  # Object containing the cached inverse
+  inv <- NULL
+  #Function that set the new matrix elements
+  set <- function(y) {
+    # The matrix is stored in the 'x' object of the parent environment
+    x <<- y
+    # The cached inverse is set to NULL
+    inv <<- NULL
+  } 
+  #Function that retrieves the matrix elements
+  get <- function() x
+  #Function that sets the inverse elements
+  setinverse <- function(inverse) inv <<- inverse
+  #Function that retrieves the inverse elements
+  getinverse <- function() inv
+  #Return the list containing the set/get functions
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse )
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of the special matrix 'x',
+## created by makeCachedMatrix. The inverse operation is performed
+## only if its result is NOT cached within the matrix 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    return(inv)
+  } else {
+    message("Performing inverse computation...")
+    inv <- solve(x$get(),...)
+    x$setinverse(inv)
+    return(inv)
+  }
 }
